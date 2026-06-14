e3ac7e2153d8c1611d0adc10c21be2a8722f9f36
SHAYAK SADHUKHAN <shayaksadhukhan402@gmail.com>
2026-06-14 07:26:21 +0530

    Add files via upload

---
+++ chatapp/main.py
@@ -0,0 +1,9 @@
+import os
+from app import create_app, socketio
+
+app = create_app()
+
+if __name__ == '__main__':
+    # local e run korar jonno
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
